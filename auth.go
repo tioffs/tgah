aac0e7721b4d5b2ef1c1ce22c452e122d7e8024f
tioffs <timofeev.pro@gmail.com>
2021-10-12 03:46:16 +0300

    add user last_name

--- auth.go
+++ auth.go
@@ -48,6 +48,7 @@ const (
 type User struct {
 	ID        int    `json:"id"`
 	FirstName string `json:"first_name"`
+	LastName  string `json:"last_name"`
 	Username  string `json:"username"`
 	PhotoURL  string `json:"photo_url"`
 	AuthDate  int    `json:"auth_date"`
